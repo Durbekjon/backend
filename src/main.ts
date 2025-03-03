8ae711e141b10bb523b90547c82930772f9a5a49
Durbek <saydaliyevdurbek0512@gmail.com>
2025-03-04 01:57:22 +0500

    cors

--- src/main.ts
+++ src/main.ts
@@ -9,18 +9,18 @@ async function bootstrap() {
 
   dotenv.config();
   app.enableShutdownHooks();
-  app.enableCors({
-    origin: (origin, callback) => {
-      if (!origin || origin === process.env.FRONTEND_IP) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-  });
   app.enableVersioning({ type: VersioningType.URI });
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.setGlobalPrefix('api');
+
+  // CORS ni faqat ma'lum IP uchun ruxsat berish
+  app.use((req, res, next) => {
+    const allowedIp = process.env.FRONTEND_IP;
+    if (req.ip !== allowedIp) {
+      return res.status(403).json({ message: 'Not allowed by CORS' });
+    }
+    next();
+  });
 
   await swaggerInit(app, process.env.APP_MODE);
   await app.listen(4000);
